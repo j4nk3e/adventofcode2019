e259cdf01b3dfaafaf5611e16b51d78150459561
juumixx <juumixx@gmail.com>
2019-11-25 21:00:19 +0100

    project setup

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:adventofcode2019/adventofcode2019.dart' as adventofcode2019;
+
+main(List<String> arguments) {
+  print('Hello world: ${adventofcode2019.calculate()}!');
+}
---
+++ test/adventofcode2019_test.dart
@@ -0,0 +1,8 @@
+import 'package:adventofcode2019/adventofcode2019.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
