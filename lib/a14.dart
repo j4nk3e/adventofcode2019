d3454bac1a7190318415dbd6d0aeb277d50d22f2
Julian Köpke <julian.koepke@gmail.com>
2019-12-15 16:52:16 +0100

    December 14. pt. 2

--- lib/a14.dart
+++ lib/a14.dart
@@ -1,46 +1,80 @@
+import 'dart:math';
+
 import 'package:adventofcode2019/a.dart';
 
 class A14 extends A {
   int one(List<String> input) {
-    var reactions = input.map((l) {
+    var reactions = Map.fromEntries(input.map((l) {
       var s = l.split(' => ');
       var i = s.first.split(', ').map((c) => Chem.from(c));
       var c = Chem.from(s.last);
       c.input = Map.fromEntries(i.map((i) => MapEntry(i.id, i.output)));
-      return c;
-    });
-    Chem fuel = reactions.firstWhere((c) => c.id == 'FUEL');
-    var re = fuel.input;
-    re['ORE'] = re['ORE'] ?? 0;
-    var pool = <String, int>{};
-    while (true) {
-      if (re.length == 1 && re.keys.first == 'ORE') {
-        print(pool);
-        return re.values.first;
-      }
-      var replace = re.entries.firstWhere((i) => i.key != 'ORE');
-      var c = reactions.firstWhere((c) => c.id == replace.key);
-      for (var add in c.input.entries) {
-        var req = add.value;
-        var count = replace.value * req;
-        if (count % c.output > 0) {
-          re[add.key] = (re[add.key] ?? 0) + 1;
-          pool[c.id] = (pool[c.id] ?? 0) + count % c.output;
+      return MapEntry(c.id, c);
+    }));
+    reactions['FUEL'].req = 1;
+    int ore = 0;
+    while (!reactions.values.every((r) => r.req <= 0)) {
+      final r = reactions.values.firstWhere((r) => r.req > 0);
+      while (r.req > 0) {
+        for (var i in r.input.entries) {
+          if (i.key == 'ORE') {
+            ore += i.value;
+          } else {
+            reactions[i.key].req += i.value;
+          }
         }
-        re[add.key] = (re[add.key] ?? 0) + (count ~/ c.output);
+        r.req -= r.output;
       }
-      re.remove(replace.key);
     }
+    return ore;
   }
 
   int two(List<String> input) {
-    return 0;
+    var reactions = Map.fromEntries(input.map((l) {
+      var s = l.split(' => ');
+      var i = s.first.split(', ').map((c) => Chem.from(c));
+      var c = Chem.from(s.last);
+      c.input = Map.fromEntries(i.map((i) => MapEntry(i.id, i.output)));
+      return MapEntry(c.id, c);
+    }));
+    var fuel = 1;
+    var tooHigh = 0;
+    while (true) {
+      reactions['FUEL'].req = fuel;
+      int ore = 0;
+      while (!reactions.values.every((r) => r.req <= 0)) {
+        final r = reactions.values.firstWhere((r) => r.req > 0);
+        var k = r.req ~/ r.output;
+        if (r.req % r.output > 0) {
+          k++;
+        }
+        for (var i in r.input.entries) {
+          if (i.key == 'ORE') {
+            ore += i.value * k;
+          } else {
+            reactions[i.key].req += i.value * k;
+          }
+        }
+        r.req -= r.output * k;
+      }
+      if (ore <= 1000000000000) {
+        if (tooHigh == fuel + 1) {
+          return fuel - 2;
+        }
+        fuel += max(1, 1000000000000 / ore * fuel * 0.001).floor();
+      } else if (ore > 1000000000000) {
+        tooHigh = fuel;
+        fuel--;
+      }
+    }
   }
 }
 
 class Chem {
   final String id;
   int output;
+  int req = 0;
+
   Map<String, int> input;
 
   Chem(this.id, this.output);
